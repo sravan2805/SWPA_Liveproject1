b8d49781fab1fc78ecc1a59054af12142583dae8
BhavyaSreeJallolla <bhavyasreejallolla967@gmail.com>
2024-11-01 21:13:02 +0530

    saran_anna

--- src/Components/Delicious_menu/Delicious_menu.jsx
+++ src/Components/Delicious_menu/Delicious_menu.jsx
@@ -14,7 +14,7 @@ const Delicious_menu = () => {
             <p>Chicken biryani, Campfire Spl. Biryani, Mutton Biryani, Mushroom Biryani, Tandoori Chicken Biryani</p>
           </div>
         </div>
-        <div className="menu-item soup-item"> {/* Add a specific class for the soups item */}
+        <div className="menu-item soup-item"> 
           <div className="menu-text">
             <h3>Soups</h3>
             <p>Chicken Manchow Soup, Sweet Corn Chicken Soup, Hot & Sour Chicken Soup</p>
