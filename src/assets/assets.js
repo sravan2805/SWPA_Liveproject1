bbcf91a8b7a4138ad732ff5d55ed856ce51d2624
D.Rupa sree <rupasree7417@gmail.com>
2024-11-01 12:27:52 +0530

    first commit

---
+++ src/assets/assets.js
@@ -0,0 +1,476 @@
+//biryani,friedrice,soups
+// main course-nonveg,veg,tandoori,starters-veg,nonveg,spl items,
+import four_am_biryani from './four_am_biryani.jpg'
+import About_us_image from './About_us_image.jpg'
+import Biryani_non_veg_img1 from './Biryani_non_veg_img1.png'
+import Biryani_non_veg_img2 from './Biryani_non_veg_img2.png'
+import Biryani_non_veg_img3 from './Biryani_non_veg_img3.png'
+import Biryani_non_veg_img4 from './Biryani_non_veg_img4.png'
+import Biryani_non_veg_img5 from './Biryani_non_veg_img5.png'
+import Biryani_non_veg_img6 from './Biryani_non_veg_img6.png'
+import Biryani_non_veg_img7 from './Biryani_non_veg_img7.png'
+import Biryani_non_veg_img8 from './Biryani_non_veg_img8.png'
+import Biryani_non_veg_img9 from './Biryani_non_veg_img9.png'
+import Biryani_non_veg_img10 from './Biryani_non_veg_img10.png'
+import Biryani_non_veg_img11 from './Biryani_non_veg_img11.png'
+import Biryani_non_veg_img12 from './Biryani_non_veg_img12.png'
+import Biryani_non_veg_img13 from './Biryani_non_veg_img13.png'
+import Biryani_non_veg_img14 from './Biryani_non_veg_img14.png'
+import Biryani_veg_img1 from './Biryani_veg_img1.png'
+import Biryani_veg_img2 from './Biryani_veg_img2.png'
+import Biryani_veg_img3 from './Biryani_veg_img3.png'
+import Biryani_veg_img4 from './Biryani_veg_img4.png'
+import Biryani_veg_img5 from './Biryani_veg_img5.png'
+import Campfire_logo_final from './Campfire_logo_final.png'
+import Delicious_menu_biriyani from './Delicious_menu_biriyani.jpg'
+import Delicious_menu_soup from './Delicious_menu_soup.jpg'
+import Delicious_menu_tandoori from './Delicious_menu_tandoori.jpg'
+import Friedrice_non_veg_img1 from './Friedrice_non_veg_img1.png'
+import Friedrice_non_veg_img2 from './Friedrice_non_veg_img2.jpeg'
+import Friedrice_non_veg_img3 from './Friedrice_non_veg_img3.jpeg'
+import Friedrice_non_veg_img4 from './Friedrice_non_veg_img4.jpg'
+import Friedrice_non_veg_img5 from './Friedrice_non_veg_img5.jpeg'
+import Friedrice_non_veg_img6 from './Friedrice_non_veg_img6.jpeg'
+import Friedrice_veg_img1 from './Friedrice_veg_img1.png'
+import Friedrice_veg_img2 from './Friedrice_veg_img2.jpeg'
+import Friedrice_veg_img3 from './Friedrice_veg_img3.jpeg'
+import Friedrice_veg_img4 from './Friedrice_veg_img4.jpeg'
+import Friedrice_veg_img5 from './Friedrice_veg_img5.jpeg'
+import Friedrice_veg_img6 from './Friedrice_veg_img6.jpeg'
+import Friedrice_veg_img7 from './Friedrice_veg_img7.jpg'
+import Friedrice_veg_img8 from './Friedrice_veg_img8.jpeg'
+import Friedrice_veg_img9 from './Friedrice_veg_img9.jpeg'
+import Friedrice_veg_img10 from './Friedrice_veg_img10.jpg'
+import Friedrice_veg_img11 from './Friedrice_veg_img11.jpeg'
+import Friedrice_veg_img12 from './Friedrice_veg_img12.jpeg'
+import Hero_image from './Hero_image.jpg'
+import Homepage_2 from './Homepage_2.webp'
+import map from './map.jpg'
+import Review_img1 from './Review_img1.jpg'
+import Review_img2 from './Review_img2.jpg'
+import Review_img3 from './Review_img3.jpg'
+import Review_img4 from './Review_img4.jpg'
+import Soups_non_veg_img1 from './Soups_non_veg_img1.jpeg'
+import Soups_non_veg_img2 from './Soups_non_veg_img2.jpg'
+import Soups_non_veg_img3 from './Soups_non_veg_img3.jpeg'
+import Soups_non_veg_img4 from './Soups_non_veg_img4.jpeg'
+import Soups_veg_img1 from './Soups_veg_img1.jpg'
+import Soups_veg_img2 from './Soups_veg_img2.jpg'
+import star from './star.png'
+import Web_plus_academy_logo from './Web_plus_academy_logo.jpg'
+
+export const assets={ 
+    Homepage_2,Campfire_logo_final,Delicious_menu_biriyani,Delicious_menu_soup,Delicious_menu_tandoori,About_us_image,four_am_biryani
+
+}
+
+export const MenuData=[
+    {
+        _id:"1",
+        title:"Veg-biryani"
+    },
+    { 
+        _id:"2",
+        title:"Non-Veg-biryani" 
+    },
+    {
+        _id:"3",
+        title:"Veg-Friedrice "
+        
+        
+    },
+    {
+        _id:"4",
+        title:"Non-veg-Friedrice"
+        
+        
+    },
+    {
+        _id:"5",
+        title:"Veg-Soups"
+        
+        
+    },
+    {
+        _id:"6",
+        title:"Non-veg-Soups"
+        
+        
+    },
+    {
+        _id:"7",
+        title:"Reviews"   
+    },
+    {
+        _id:"8",
+        title:"hero image"  
+
+    },
+    {
+        _id:"9",
+        title:"map"   
+    },
+    {
+        _id:"10",
+        title:"star"   
+    },
+    {
+        _id:"11",
+        title:"web_plus_academy_logo"   
+    }
+
+]
+export const veg_biryani=[
+    {
+        _id:"1-1",
+        title:"Biryanirice",
+        image:[Biryani_veg_img1],
+        price:"60"
+    },
+    {
+        _id:"1-2",
+        title:"Veg Biryani",
+        image:[Biryani_veg_img2],
+        price:"80"
+    },
+    {
+        _id:"1-3",
+        title:"Kaju Biryani",
+        image:[Biryani_veg_img3],
+        price:"150"
+    },
+    {
+        _id:"1-4",
+        title:"Mushroom Biryani",
+        image:[Biryani_veg_img4],
+        price:"150"
+    },
+    {
+        _id:"1-5",
+        title:"Panner Biryani",
+        image:[Biryani_veg_img5],
+        price:"140"
+    }
+]
+export const Non_veg_biryani=[
+    {
+        _id:"2-1",
+        title:"Biryanirice",
+        image:[Biryani_non_veg_img1],
+        price:"60"
+    },
+    {
+        _id:"2-2",
+        title:"Chicken Dum Biriyani",
+        image:[Biryani_non_veg_img2],
+        price:"110"
+    },
+    {
+        _id:"2-3",
+        title:"Chickem Bonelss Biryani",
+        image:[Biryani_non_veg_img3],
+        price:"120"
+    },
+    {
+        _id:"2-4",
+        title:"Campfire Special Biryani",
+        image:[Biryani_non_veg_img4],
+        price:"130"
+    },
+    {
+        _id:"2-5",
+        title:"Fry piece Biryani",
+        image:[Biryani_non_veg_img5],
+        price:"110"
+    },
+    {
+        _id:"2-6",
+        title:"Mutton Biryani",
+        image:[Biryani_non_veg_img6],
+        price:"250"
+    },
+    {
+        _id:"2-7",
+        title:"Fish Biryani",
+        image:[Biryani_non_veg_img7],
+        price:"150"
+    },
+    {
+        _id:"2-8",
+        title:"Prawns Biryani",
+        image:[Biryani_non_veg_img8],
+        price:"220"
+    },
+    {
+        _id:"2-9",
+        title:"Egg Biryani",
+        image:[Biryani_non_veg_img9],
+        price:"80"
+    },
+    
+    {
+        _id:"2-10",
+        title:"Chicken lollipop Biryani",
+        image:[Biryani_non_veg_img10],
+        price:"150"
+    },
+    {
+        _id:"2-11",
+        title:"Chicken tikka Biryani",
+        image:[Biryani_non_veg_img11],
+        price:"270"
+    },
+    {
+        _id:"2-12",
+        title:"chicken keema Biryani",
+        image:[Biryani_non_veg_img12],
+        price:"140"
+    },
+    {
+        _id:"2-13",
+        title:"Chicken keema Biryani(full)",
+        image:[Biryani_non_veg_img13],
+        price:"270"
+    },
+    {
+        _id:"2-14",
+        title:"Tandoori chicken Biryani",
+        image:[Biryani_non_veg_img14],
+        price:"290"
+    }
+]
+export const VegFriedrice=[
+    {
+        _id:"3-1",
+        title:"Jeera rice",
+        image:[Friedrice_veg_img1],
+        price:"80"
+    },
+    {
+        _id:"3-2",
+        title:"Gobi Fried rice",
+        image:[Friedrice_veg_img2],
+        price:"80"
+    },
+    
+    {
+        _id:"3-3",
+        title:"Paneer Fried rice",
+        image:[Friedrice_veg_img3],
+        price:" 90 "
+    },
+    {
+        _id:"3-4",
+        title:"Mushroom Fried rice",
+        image:[Friedrice_veg_img4],
+        price:"100"
+    },
+    {
+        _id:"3-5",
+        title:"Kaju Fried rice",
+        image:[Friedrice_veg_img5],
+        price:"110"
+    },
+    {
+        _id:"3-6",
+        title:"Ghee rice",
+        image:[Friedrice_veg_img6],
+        price:"90"
+    },
+    {
+        _id:"3-7",
+        title:"Tomato rice",
+        image:[Friedrice_veg_img7],
+        price:"70"
+    },
+    {
+        _id:"3-8",
+        title:"Veg pulao",
+        image:[Friedrice_veg_img8],
+        price:"120"
+    },
+    {
+        _id:"3-9",
+        title:"Veg mix Friedrice",
+        image:[Friedrice_veg_img9],
+        price:"120"
+    },
+    {
+        _id:"3-10",
+        title:"Veg Friedrice",
+        image:[Friedrice_veg_img10],
+        price:"70"
+    },
+    {
+        _id:"3-11",
+        title:" Baby corn Friedrice",
+        image:[Friedrice_veg_img11],
+        price:"90"
+    },
+    {
+        _id:"3-12",
+        title:"Veg schezwan Friedrice",
+        image:[Friedrice_veg_img12],
+        price:"100"
+    },
+    {
+        _id:"3-13",
+        title:"Egg Friedrice",
+        image:[Friedrice_non_veg_img1],
+        price:"80"
+    },
+]
+export const nonvegFriedrice=[
+    
+    {
+        _id:"4-1",
+        title:"Chicken Friedrice",
+        image:[Friedrice_non_veg_img2],
+        price:"110"
+    },
+    {
+        _id:"4-2",
+        title:"Mutton Friedrice",
+        image:[Friedrice_non_veg_img3],
+        price:"150"
+    },
+    {
+        _id:"4-3",
+        title:"Non-veg mix Friedrice",
+        image:[Friedrice_non_veg_img4],
+        price:"150"
+    },
+    {
+        _id:"4-4",
+        title:"Chicken schezwan Friedrice",
+        image:[Friedrice_non_veg_img5],
+        price:"120"
+    },
+    {
+        _id:"4-5",
+        title:"Mutton schezwan Friedrice",
+        image:[Friedrice_non_veg_img6],
+        price:"160"
+    },
+]
+export const vegsoups=[
+    {
+        _id:"5-1",
+        title:"Tomato Soup",
+        image:[Soups_veg_img1],
+        price:"60"
+    },
+    {
+        _id:"5-2",
+        title:"Panner chat pat Soup",
+        image:[Soups_veg_img2],
+        price:"90"
+    }
+]
+export const nonvegsoups=[
+    {
+        _id:"6-1",
+        title:"Chicken hot & sour Soups",
+        image:[Soups_non_veg_img1],
+        price:"80"
+    },
+    {
+        _id:"6-2",
+        title:"Mutton hot & sour Soups",
+        image:[Soups_non_veg_img2],
+        price:"100"
+    },
+    {
+        _id:"6-3",
+        title:"Chicken manchow Soup",
+        image:[Soups_non_veg_img3],
+        price:"90"
+    },
+    {
+        _id:"6-4",
+        title:"Mutton manchow Soup",
+        image:[Soups_non_veg_img4],
+        price:"120"
+    }
+    
+]
+export const reviews=[
+    {
+        _id:"7-1",
+        title:"Review1",
+        image:[Review_img1],
+        
+    },
+    {
+        _id:"7-2",
+        title:"Review2",
+        image:[Review_img2],
+        
+    },
+    {
+        _id:"7-3",
+        title:"Review3",
+        image:[Review_img3],
+        
+    },
+    {
+        _id:"7-4",
+        title:"Review4",
+        image:[Review_img4],
+        
+    }
+]
+export const heroimage=[
+    {
+        _id:"8-1",
+        title:"HeroImage",
+        image:[Hero_image],
+        
+    }
+]
+export const map=[
+    {
+        _id:"9-1",
+        title:"Map",
+        image:[Map.jpg],
+        
+    }
+]
+export const star=[
+    {
+        _id:"10-1",
+        title:"star",
+        image:[star.png],
+        
+    }
+]
+export const Web_plus_academy_logologo=[
+    {
+        _id:"11-1",
+        title:"web_plus_academy_logo",
+        image:[Web_plus_academy_logo.jpg],
+        
+    }
+]
+
+
+
+
+
+
+
+
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+
+
+
